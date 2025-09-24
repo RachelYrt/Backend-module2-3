c9804f8488ba1b709f172ffee75866669795fe03
Rachel <Rachel_yrt@outlook.com>
2025-09-24 20:06:19 +1000

    module2 step1-creating one table

---
+++ Backend/ToDoApi/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
+namespace ToDoApi.Data
+{
+    public class TodoContext(DbContextOptions<TodoContext> options) : DbContext(options)
+    {
+        public DbSet<Todo> Todos => Set<Todo>();
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+
+        //}
+
+    }
+}
---
+++ Backend/ToDoApi/Models/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApi.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+        public string? Discription { get; set; }
+
+    }
+}
