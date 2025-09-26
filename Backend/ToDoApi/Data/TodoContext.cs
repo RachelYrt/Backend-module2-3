b007a1a2124a86c7a8ea920b76f016a021bb42ad
Rachel <Rachel_yrt@outlook.com>
2025-09-26 13:52:41 +1000

    Add category model which is one-to-many and seed data to test

--- Backend/ToDoApi/Data/TodoContext.cs
+++ Backend/ToDoApi/Data/TodoContext.cs
@@ -6,15 +6,25 @@ namespace ToDoApi.Data
     public class TodoContext(DbContextOptions<TodoContext> options) : DbContext(options)
     {
         public DbSet<Todo> Todos => Set<Todo>();
+        public DbSet<Category> Categories => Set<Category>();
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>()
+                .HasMany(a => a.Todos)
+                .WithOne(a => a.Category)
+                .HasForeignKey(a => a.CategoryId);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Personal" },
+                new Category { Id = 2, Name = "Work" },
+                new Category { Id = 3, Name = "Travel" });
             modelBuilder.Entity<Todo>().HasData(
                 new Todo
                 {
                     Id="1",
                     Text="buy some candy.",
                     Description = "none",
+                    CategoryId = 1
 
                 },
                 new Todo
@@ -22,6 +32,7 @@ namespace ToDoApi.Data
                     Id = "5",
                     Text = "new meeting tomorrow.",
                     Description = "alart",
+                    CategoryId = 2
 
                 },
                 new Todo
@@ -29,6 +40,7 @@ namespace ToDoApi.Data
                     Id = "8",
                     Text = "new tennis class this week.",
                     Description = "low primary",
+                    CategoryId = 1
                 }
                 );
         }
--- Backend/ToDoApi/Models/Todo.cs
+++ Backend/ToDoApi/Models/Todo.cs
@@ -5,6 +5,8 @@
         public string Id { get; set; }=Guid.NewGuid().ToString();
         public string Text { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public Category? Category { get; set; }
+        public int? CategoryId { get; set; }
 
     }
 }
