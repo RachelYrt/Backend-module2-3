d860dad06a3d6aaba989dd372cd95cbe8a86f1ce
Rachel <Rachel_yrt@outlook.com>
2025-09-28 14:40:08 +1000

    refactored the controller by seperating CRUD logic to new service layer, and introduced DTOs with mapping to entity models from API reponses. and applied dependency injection to controller.

---
+++ Backend/ToDoApi/DTOs/TodoDto.cs
@@ -0,0 +1,13 @@
+﻿using ToDoApi.Models;
+
+namespace ToDoApi.DTOs
+{
+    public class TodoDto
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Text { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int? CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
